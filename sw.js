f745210e9f4711ac07857145b28d2b1ac3c2d45c
Karl West <karl.west@viaecole.se>
2018-10-19 12:43:35 +0200

    added service worker

--- sw.js
+++ sw.js
@@ -1,3 +1,55 @@
+var staticCacheName = 'mws-restaurant-v1';
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(staticCacheName).then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/restaurant.html',
+                '/css/styles.css',
+                '/js/app.js',
+                '/js/dbhelper.js',
+                '/js/main.js',
+                '/js/restaurant_info.js',
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg',
+                '/data/restaurants.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName.startsWith('mws-restaurant-') &&
+                        cacheName != staticCacheName;
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
 self.addEventListener('fetch', function (event) {
-    console.log('hellosw');
+    if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
+        return;
+    }
+    event.respondWith(
+        caches.match(event.request, { ignoreSearch: true }).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
 });
